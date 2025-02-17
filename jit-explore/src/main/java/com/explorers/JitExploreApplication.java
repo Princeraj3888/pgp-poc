b1a0962179e531cf76acde0027a865df116502e2
rajlomatech2023 <raj@lomatechnology.com>
2025-02-17 18:58:36 +0700

    initial commit

---
+++ jit-explore/src/main/java/com/explorers/JitExploreApplication.java
@@ -0,0 +1,13 @@
+package com.explorers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JitExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JitExploreApplication.class, args);
+	}
+
+}
---
+++ jit-explore/src/test/java/com/explorers/JitExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explorers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JitExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
